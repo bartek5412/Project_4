d04b6c58fc4f28c0025858d0a9096b0d40c3c232
Bartosz Tokarski <bartek5412@gmail.com>
2024-10-24 21:33:01 +0200

    step 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface GreetingProps {
+  name: string;
+  age: number;
+  date?: any;
+}
+
+const currentTime = () => {
+  const today = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return today.toLocaleDateString(undefined, options);
+};
+const Greeting: React.FC<GreetingProps> = ({ name, age }) => {
+  return (
+    <div>
+      <p>
+        Hello, {name}, you have {age} years old!
+      </p>
+      <p>Today is: {currentTime()}</p>
+    </div>
+  );
+};
+
+const Counter: React.FC = () => {
+  const [cout, setCout] = useState<number>(0);
+  const removeCouter = () => {
+    if (cout > 0) {
+      setCout(cout - 1);
+    } else {
+      alert("Licznik nie może zejśc poniżej zera");
+    }
+  };
+  const dublicateCouter = () => {
+    setCout(cout * 2);
+  };
+  const setZero = () => {
+    setCout(0);
+  };
+  return (
+    <div>
+      <p>Current couter: {cout}</p>
+      <button onClick={() => setCout(cout + 1)}>Add</button>
+      <button onClick={removeCouter}>Remove</button>
+      <button onClick={dublicateCouter}>Dublicate couter</button>
+      <button onClick={setZero}>Set zero</button>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Greeting name="Jack" age={20}></Greeting>
+        <Counter></Counter>
       </header>
     </div>
   );
